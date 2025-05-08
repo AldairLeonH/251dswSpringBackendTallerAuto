375498b1e24680bdd60f37f517f40a31bd3ebde3
Ciro Pacheco <ciropacheco2002@gmail.com>
2025-05-08 16:38:36 -0500

    se añadio el api persona para crear,actualizar, eliminar, listar

--- src/main/java/dsw/tallerbackend/service/TipoDocumentoService.java
+++ src/main/java/dsw/tallerbackend/service/TipoDocumentoService.java
@@ -4,12 +4,14 @@ package dsw.tallerbackend.service;
 import dsw.tallerbackend.model.TipoDocumento;
 import dsw.tallerbackend.reporistory.TipoDocumentoRepository;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TipoDocumentoService {
+    @Autowired
     TipoDocumentoRepository TipoDocumentoRepository;
-    public List<TipoDocumento> getipoDocumentos(){
+    public List<TipoDocumento> getTipoDocumentos(){
         return TipoDocumentoRepository.findAll();
     }
     
